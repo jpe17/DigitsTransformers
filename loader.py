50ca081944f4ef3fdf0fc88542c90f54ea37e71d
Felipe Lavratti <felipelav@gmail.com>
2025-06-23 11:42:11 +0100

    updated loading paths

--- loader.py
+++ loader.py
@@ -4,7 +4,7 @@ from torchvision.datasets import MNIST
 from torchvision.transforms import transforms
 
 class MnistLoader():
-    def __init__(self, datadir = '39-data/', batch_size = 128):
+    def __init__(self, datadir = '.w3-data/', batch_size = 128):
         self.mnist_dataset = MNIST(root = datadir, download=True, train = True, transform = transforms.ToTensor())
         self.train_data, self.validation_data = random_split(self.mnist_dataset, [50000, 10000])
         self.train_loader = DataLoader(self.train_data, batch_size, shuffle = True)
